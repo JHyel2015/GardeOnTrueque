b4fc036a0a53ba6c8b5bbde76f76b1cc622f5585
JHyel2015 <elgeorh@gmail.com>
2019-07-29 15:22:39 -0500

    upload data

--- GardeonTrueque/src/app/components/new-post/new-post.component.ts
+++ GardeonTrueque/src/app/components/new-post/new-post.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DataApiService } from '../../services/data-api.service';
+import { AuthService } from '../../services/auth.service';
+import { UserInterface } from '../../models/user';
+import { PlantInterface } from '../../models/plant';
+import { AdInterface } from '../../models/ad';
 
 @Component({
   selector: 'app-new-post',
@@ -7,9 +12,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPostComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private dataapi: DataApiService) { }
+
+  user: UserInterface;
+  plant: PlantInterface = {
+    name: '',
+    type: '',
+    image: '',
+    description: '',
+    user: { id: 0},
+  };
+  ad: AdInterface = {
+    status: true,
+    plant: { id: 0},
+    user: {id: 0},
+  };
+  file: File;
+  urlimage: string;
 
   ngOnInit() {
+    this.getCurrentUser();
   }
 
+  onChange(e) {
+    this.file = e.target.files[0];
+    console.log(e);
+  }
+
+  uploadImg() {
+    this.dataapi.uploadImage(this.file, 'uploads/' + this.file.name, '')
+      .subscribe( url => this.urlimage = url);
+  }
+
+  getCurrentUser() {
+    this.authService.isAuth().subscribe( auth => {
+      if (auth) {
+        this.user = auth;
+        this.dataapi.getUser(this.user.uid)
+          .subscribe(
+            result => {
+              this.user = result;
+              const date = new Date(result.createdAt.toString());
+              this.user.createdAt = date.getFullYear() +
+              '-' + (date.getUTCMonth() + 1) +
+              '-' + date.getUTCDate() +
+              ' ' + date.toLocaleTimeString();
+            }, error => {
+              console.log(error.message);
+            }
+          );
+      } else {
+      }
+    });
+  }
+
+  uploadData() {
+    if (this.file !== null) {
+      this.plant.user.id = this.user.id;
+      this.dataapi.uploadImage(this.file, 'uploads/' + this.file.name, '')
+        .subscribe( url => {
+          this.plant.image = url;
+          this.dataapi.savePlant(this.plant)
+            .subscribe(plantSaved => {
+              this.plant = plantSaved.plant;
+              this.ad.plant.id = this.plant.id;
+              this.ad.user.id = this.user.id;
+              this.dataapi.saveAd(this.ad)
+                .subscribe(adSaved => {
+                  console.log(adSaved);
+                }, err => console.log(err));
+            }, err => console.log(err));
+        });
+    }
+  }
 }
